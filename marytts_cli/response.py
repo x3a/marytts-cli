2f43202af8ab31ccb772921154c34b5eb102941c
trevor <trevor@destroyed.today>
2020-03-19 02:34:55 +0100

    Add response.py
    Added representation of MaryTTS HTTP server responses.

---
+++ marytts_cli/response.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Data structure to represent responses received from the MaryTTS server"""
+
+import collections
+
+PROPERTIES = [
+    'content',
+    'headers',
+    'ok',
+    'reason',
+    'status'
+]
+
+class MaryTTSResponse(collections.namedtuple('MaryTTSResponse', PROPERTIES)):
+    """Representation of a response from the MaryTTS server"""
