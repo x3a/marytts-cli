d97c3903c8cf7700f94f481758082a6b8a5dcb9f
trevor <trevor@destroyed.today>
2020-03-20 01:30:04 +0100

    Add defaults.py
    Added fallback defaults for request parameters of the client.

---
+++ marytts_cli/defaults.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Fallback defaults of the request parameters used by the MaryTTS client"""
+
+DEFAULT_QUERY_URL = 'http://127.0.0.1:59125/process'
+DEFAULT_INPUT_TYPE = 'TEXT'
+DEFAULT_OUTPUT_TYPE = 'AUDIO'
+DEFAULT_AUDIO = 'WAVE_FILE'
+DEFAULT_LOCALE = 'en_GB'
+DEFAULT_VOICE = 'dfki-prudence-hsmm'
